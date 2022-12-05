ee65220e180c2122051ea93e0294f38e49d76ce4
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-05 20:32:31 +0530

    Update Area_of_a_room.cpp

--- Expressions/Area_of_a_room.cpp
+++ Expressions/Area_of_a_room.cpp
@@ -2,10 +2,5 @@
 using namespace std;
 int main()
 {
-	float width,length;
-	cout<<"Enter Width";
-	cin>>width;
-	cout<<"Enter Length:";
-	cin>>length;
-	cout<<"Area of a Room is "<<width*length<<"sqft";
+	cout<<"hello";
 }
