2fc7ce972475b5bfc98f9ddd5f219223d84137dd
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-10 20:09:58 +0530

    Create 1.Mailing_Address.cpp

---
+++ Expressions/1.Mailing_Address.cpp
@@ -0,0 +1,11 @@
+/*Write a program that displays your name and complete mailing address.
+The address should be printed in the format that is normally used in the area where you live.
+Your program does not need to read any input from the user.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Name:Ankitha"<<"\n";
+	cout<<"Address:1-23-456";
+}
