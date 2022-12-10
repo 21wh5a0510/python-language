f55b25b966f2f8656602d47b154f9a7543cd598c
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-10 22:55:31 +0530

    Update 4.Area of a Triangle.cpp

--- Expressions/4.Area of a Triangle.cpp
+++ Expressions/4.Area of a Triangle.cpp
@@ -1,3 +1,7 @@
+/*Write a program that computes the area of a triangle when the lengths of all three sides are known.
+Let s1,s2,s3 be the lengths of the sides and let s=(s1+s2+s3)/2. Then the area of the triangle can be
+calculated using the following formula:area=sqrt(s*(s-s1)*(s-s2)*(s-s3)).*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
