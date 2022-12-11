4f8025f7376038ce308348634ebfae56acda11cf
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-11 14:16:50 +0530

    Update 6.Tax and Tip.cpp

--- Expressions/6.Tax and Tip.cpp
+++ Expressions/6.Tax and Tip.cpp
@@ -1,7 +1,7 @@
 /*The program that you create for this exercise will begin by reading the cost of a meal ordered at a restaurant from the user.
 Then your program will compute the tax and tip for the meal. Compute the tax as 18 percent of the meal amount.Compute the tip as 
 5 percent of the meal amount (without the tax). The output from your program should include the tax amount,the tip amount, and the
-grand total for the meal including both the tax and the tip. Format the output so that all of the values are displayed using two decimal places*/
+grand total for the meal including both the tax and the tip. Format the output so that all of the values are displayed using two decimal places.*/
 
 #include<bits/stdc++.h>
 using namespace std;
