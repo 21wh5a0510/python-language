95a9a9da851e1344458d0bcb5cba00c47db27874
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-12 23:04:42 +0530

    Update 1.Sum of Integers.cpp

--- 3.Repetitions/Simple Loops/1.Sum of Integers.cpp
+++ 3.Repetitions/Simple Loops/1.Sum of Integers.cpp
@@ -12,3 +12,22 @@ int main()
 	cout<<"The Sum of first "<<n<<" integers are "<<sum;
 	return 0;
 }
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,sum=0,i=1;
+	cout<<"Enter Number:";
+	cin>>n;
+	while(n>0)
+	{
+		sum+=i;
+		i++;
+		n--;
+	}
+	cout<<"The Sum of first integers are "<<sum;
+	return 0;
+}*/
