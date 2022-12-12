92ac4a1102a60777b11f07a4615da7871e8a6b06
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-12 19:02:07 +0530

    Create 7.Season from Month and Day.cpp

---
+++ Decision Making/7.Season from Month and Day.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string month;
+	int day;
+	cout<<"Enter month:";
+	cin>>month;
+	cout<<"Enter day:";
+	cin>>day;
+	if(month=="January"||month=="February")
+	{
+		cout<<"Spring";
+	}
+	else if((month=="December"&&day>21)||(month=="March"&&day<=20))
+	{
+		cout<<"Spring";
+	}
+	else if(month=="April"||month=="June")
+	{
+		cout<<"Summer";
+	}
+	else if((month=="March"&&day>20)||(month=="June"&&day<=21))
+	{
+		cout<<"Summer";
+	}
+	else if(month=="July"||month=="August")
+	{
+		cout<<"Fall";
+	}
+	else if((month=="June"&&day>21)||(month=="September"&&day<=22))
+	{
+		cout<<"Fall";
+	}
+	else if(month=="October"||month=="November")
+	{
+		cout<<"Winter";
+	}
+	else if((month=="September"&&day>22)||month=="December"&&day<=21)
+	{
+		cout<<"Winter";
+	}
+	else
+	{
+		cout<<"Enter correct month";
+	}
+}
