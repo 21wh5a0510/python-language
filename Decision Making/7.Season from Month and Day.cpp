383f1c09d1ddd98eb5c3fed64d22c1f0293c98af
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-12 19:41:05 +0530

    Update 7.Season from Month and Day.cpp

--- Decision Making/7.Season from Month and Day.cpp
+++ Decision Making/7.Season from Month and Day.cpp
@@ -1,3 +1,18 @@
+/*The year is divided into four seasons: spring, summer, fall (or autumn) and winter.
+While the exact dates that the seasons change vary a little bit from year to year because 
+of the way that the calendar is constructed, we will use the following dates for this exercise:
+
+                 Season            First Day
+				                  
+			     Spring       	    March 20
+                 Summer             June 21
+                  Fall             September 22
+                 Winter             December 21
+
+Create a program that reads a month and day from the user. The user will enter the name of the month as a string,
+followed by the day within the month as an integer. Then your program should display the season associated with 
+the date that was entered.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
