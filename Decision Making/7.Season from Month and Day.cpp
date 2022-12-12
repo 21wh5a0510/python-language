eda6d6c179991809ea6f70bf3c25b8f10eb2aad0
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-12 19:47:08 +0530

    Update 7.Season from Month and Day.cpp

--- Decision Making/7.Season from Month and Day.cpp
+++ Decision Making/7.Season from Month and Day.cpp
@@ -1,9 +1,8 @@
 /*The year is divided into four seasons: spring, summer, fall (or autumn) and winter.
 While the exact dates that the seasons change vary a little bit from year to year because 
 of the way that the calendar is constructed, we will use the following dates for this exercise:
-
                  Season            First Day
-				   Spring       	     March 20
+				 Spring       	     March 20
                  Summer             June 21
                   Fall             September 22
                  Winter             December 21
