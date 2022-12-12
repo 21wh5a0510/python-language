0a4fc2df45f60e69bfeceb2b17be922cbee9bfc4
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-12 06:31:32 +0530

    Update 5.License Plate Valid or Not.cpp

--- Decision Making/5.License Plate Valid or Not.cpp
+++ Decision Making/5.License Plate Valid or Not.cpp
@@ -14,6 +14,29 @@ int main()
 	cin>>s;
 	if(s.length()==6)
 	{
-		if(s[0]>='A'&&s[0]<='Z'&&)
+		if(s[0]>='A' && s[0]<='Z' && s[1]>='A' && s[1]<='Z' && s[2]>='A' && s[2]<='Z' && s[3]>='0' && s[3]<='9' && s[4]>='0' && s[4]<='9' && s[5]>='0' && s[5]<='9')
+		{
+			cout<<"License plate is older style and it is valid";
+		}
+		else
+		{
+			cout<<"License plate is not valid";
+		}
 	}
+	else if(s.length())
+	{
+		if(s[0]>='0' && s[0]<='9' && s[1]>='0' && s[1]<='9' && s[2]>='0' && s[2]<='9' && s[3]>='0' && s[3]<='9' && s[4]>='A' && s[4]<='Z' && s[5]>='A' && s[5]<='Z' && s[6]>='A' && s[6]<='Z')
+		{
+			cout<<"License plate is newer style and it is valid";
+		}
+		else
+		{
+			cout<<"License plate is not valid";
+		}
+	}
+	else
+	{
+		cout<<"License plate is not valid";
+	}
+	return 0;
 }
