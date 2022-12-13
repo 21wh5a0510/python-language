fa02e04b00f68de021b4a878adf4ed92181711bb
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-13 19:11:56 +0530

    Update 5.Sum of Odd Integers.cpp

--- 3.Repetitions/Simple Loops/5.Sum of Odd Integers.cpp
+++ 3.Repetitions/Simple Loops/5.Sum of Odd Integers.cpp
@@ -13,3 +13,22 @@ int main()
 	cout<<sum;
 	return 0;
 }
+
+/* Using While
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,sum=0,odd=1;
+	cout<<"Enter n:";
+	cin>>n;
+	while(n>0)
+	{
+		sum+=odd;
+		odd+=2;
+		n--;
+	}
+	cout<<sum;
+	return 0;
+}
