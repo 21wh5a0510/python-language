bd41ee7c781d35e11e0e0d899ec69d5a553c8fbb
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-13 06:50:10 +0530

    Update 3.Sum of the Digits.cpp

--- 3.Repetitions/Simple Loops/3.Sum of the Digits.cpp
+++ 3.Repetitions/Simple Loops/3.Sum of the Digits.cpp
@@ -1,3 +1,6 @@
+/*Write a program that reads an integer from the user and displays the sum of its digits.
+For example,if the user enters 31415 then your program should display 3+1+4+1+5=14.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
