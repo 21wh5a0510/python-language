3ec54e61ce7f3ce7047439dfc2a8aea74eee6832
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-14 16:10:06 +0530

    Create 10.Decimal to Binary.cpp

---
+++ 3.Repetitions/Simple Loops/10.Decimal to Binary.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string result="";
+	int q;
+	cin>>q;
+	while(q!=0)
+	{
+		int r=q%2;
+		result=to_string(r)+result;
+		q=q/2;
+	}
+	cout<<result;
+}
