2a9a21a2e537c652ab090f8e54eff4e2222d00eb
21wh5a0510 <93467117+21wh5a0510@users.noreply.github.com>
2022-12-14 06:37:12 +0530

    Update 5.Sum of Odd Integers.cpp

--- 3.Repetitions/Simple Loops/5.Sum of Odd Integers.cpp
+++ 3.Repetitions/Simple Loops/5.Sum of Odd Integers.cpp
@@ -1,3 +1,5 @@
+/*Given the value of n write a program that displays the sum of first n odd integers.For given value of 3 the program should display 1+3+5=9.*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
